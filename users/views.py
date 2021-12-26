2eeddd5c2c0c71b19960dc99bdb346b3615acae0
Naima Hassan <naima.hassan434@gmail.com>
2021-12-26 23:21:08 +0000

    User registration

--- users/views.py
+++ users/views.py
@@ -4,10 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm
 # Create your views here.
 
 def loginUser(request):
-
+   page='login'
    if request.user.is_authenticated:
       return redirect('profiles')
 
@@ -34,6 +35,27 @@ def logoutUser(request):
    logout(request)
    messages.error(request,'Username was logged out!')
    return redirect('login')
+
+def registerUser(request):
+   page='register'
+   form = CustomUserCreationForm()
+
+   if request.method == 'POST':
+      form = CustomUserCreationForm(request.POST)
+      if form.is_valid():
+         user = form.save(commit = False)
+         user.username = user.username.lower()
+         user.save()
+
+         messages.success(request, 'User account was successfully registered!')
+
+         login(request, user)
+         return redirect('profiles')
+      else:
+         messages.success(request, 'An error occurred while registering!')
+
+   context ={'page':page, 'form':form}
+   return render(request, 'users/login_register.html', context)
 
 def profiles(request):
    profiles = Profile.objects.all()
