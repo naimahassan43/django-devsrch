4c039f131ae5e3a3c5e33a3e174c5ebe756f2628
Naima Hassan <naima.hassan434@gmail.com>
2021-12-29 22:18:40 +0000

    Delete  Skill

--- users/views.py
+++ users/views.py
@@ -129,3 +129,15 @@ def updateSkill(request, pk):
 
    context = {'form': form}
    return render(request, 'users/skill_form.html', context)
+
+def deleteSkill(request,pk):
+   profile = request.user.profile
+   skill = profile.skill_set.get(id=pk)
+
+   if request.method == 'POST':
+      skill.delete()
+      messages.warning(request, 'Skill was deleted successfully!')
+      return redirect('account')
+
+   context = {'object':skill}
+   return render(request, 'delete_temp.html', context)
