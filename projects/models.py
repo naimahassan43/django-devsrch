b44f4f19dadd496bf89c2bdc0121415fa949b366
Naima Hassan <naima.hassan434@gmail.com>
2021-12-30 00:07:59 +0000

    Ordering projects by created

--- projects/models.py
+++ projects/models.py
@@ -12,20 +12,23 @@ class Tag(models.Model):
    return self.name
 
 class Projects(models.Model):
- owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
- title = models.CharField(max_length=200)
- description = models.TextField(null=True, blank=True)
- featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
- demo_link = models.CharField(max_length=2000,null=True, blank=True)
- source_link = models.CharField(max_length=2000,null=True, blank=True)
- tags = models.ManyToManyField(Tag, blank=True)
- vote_total = models.IntegerField(default=0,null=True, blank=True)
- vote_ratio = models.IntegerField(default=0,null=True, blank=True)
- created_at = models.DateTimeField(auto_now_add=True)
- id = models.UUIDField(default = uuid.uuid4, unique=True, primary_key=True, editable=False)
+  owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
+  title = models.CharField(max_length=200)
+  description = models.TextField(null=True, blank=True)
+  featured_image = models.ImageField(null=True, blank=True, default="default.jpg")
+  demo_link = models.CharField(max_length=2000,null=True, blank=True)
+  source_link = models.CharField(max_length=2000,null=True, blank=True)
+  tags = models.ManyToManyField(Tag, blank=True)
+  vote_total = models.IntegerField(default=0,null=True, blank=True)
+  vote_ratio = models.IntegerField(default=0,null=True, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  id = models.UUIDField(default = uuid.uuid4, unique=True, primary_key=True, editable=False)
 
- def __str__(self):
-  return self.title
+  def __str__(self):
+   return self.title
+ 
+  class Meta:
+   ordering = ['-created_at']
 
 class Review(models.Model):
  VOTE_TYPE = (
