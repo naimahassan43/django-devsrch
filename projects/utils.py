5d90c0c78918c42e5d9f5dd1490e286a6ad63e9b
Naima Hassan <naima.hassan434@gmail.com>
2021-12-31 01:17:51 +0000

    Reviews

--- projects/utils.py
+++ projects/utils.py
@@ -6,6 +6,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def paginateProjects(request, projects, results):
    page = request.GET.get('page')
+   results = 3
    paginator = Paginator(projects, results)
 
    try:
