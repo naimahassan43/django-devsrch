671ed1db0274751b19e76f796e704ceef39d64ae
Naima Hassan <naima.hassan434@gmail.com>
2022-01-03 16:03:11 +0000

    Django REST Framework & Serializers

--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
-from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProjectsSerializer
+from projects.models import Projects
+
+@api_view(['GET'])
 def getRoutes(request):
 
    routes = [
@@ -9,8 +14,17 @@ def getRoutes(request):
 
       {'POST': '/api/users/token'},
       {'POST': '/api/users/token/refresh'},
-
-      
    ]
+   return Response(routes)
 
-   return JsonResponse(routes, safe=False)
+@api_view(['GET'])
+def getProjects(request):
+   projects = Projects.objects.all()
+   serializer = ProjectsSerializer(projects, many=True)
+   return Response(serializer.data)
+
+@api_view(['GET'])
+def getProject(request, pk):
+   project = Projects.objects.get(id=pk)
+   serializer = ProjectsSerializer(project, many=False)
+   return Response(serializer.data)
