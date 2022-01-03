0b372fa7fa3b82d83c006cd7e9981b946ec2df77
Naima Hassan <naima.hassan434@gmail.com>
2022-01-03 18:16:40 +0000

    Nested Serializers & Serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,34 @@
 from rest_framework import serializers
-from projects.models import Projects
+from projects.models import Projects, Tag, Review
+from users.models import Profile
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
 class ProjectsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Projects
-        fields = '__all__'
+   owner = ProfileSerializer(many=False)
+   tags = TagSerializer(many=True)
+   reviews = serializers.SerializerMethodField()
+   
+   class Meta:
+      model = Projects
+      fields = '__all__'
+
+   def get_reviews(self, obj):
+      reviews = obj.review_set.all()
+      serializer = ReviewSerializer(reviews, many=True)
+      return serializer.data
+      
