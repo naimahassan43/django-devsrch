d5ad8dd8d68085e739a851f91c46842b298e651d
Naima Hassan <naima.hassan434@gmail.com>
2022-03-02 21:13:30 +0000

    Few changes

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -125,6 +125,8 @@ WSGI_APPLICATION = 'devsearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+#
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
